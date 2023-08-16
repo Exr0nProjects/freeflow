50e0be58af0632c781dbec6d9722df0a81445941
Exr0n <howdy@exr0n.com>
2023-08-16 12:59:34 -0700

    i should just use apache spark :(

--- free_flow/lib.py
+++ free_flow/lib.py
@@ -89,6 +89,6 @@ if __name__ == '__main__':
 
     # ---- process ---- + --- another fn --+
     #                   |                  |
-    #                   +---- something ---+----->
+    #                   +---- something ---+--- batch -->
 
 
